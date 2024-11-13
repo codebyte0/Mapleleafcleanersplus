84c98d6de9caf45a8d4502cbe577b96e7f3e1833
codebyte0 <codebyte.wd@gmail.com>
2024-11-12 21:30:55 -0800

    12/11

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -2,12 +2,12 @@ import Link from "next/link";
 import React from "react";
 import {
   FaFacebook,
-  FaMobileAlt,
   FaGlobeAmericas,
   FaMailBulk,
 } from "react-icons/fa";
 import Image from "next/image";
 import logo from "../assets/logo.png";
+import useEdmontonAreaDetection from "../hooks/useCityName";
 
 const Footer = () => {
   const links = [
@@ -16,78 +16,79 @@ const Footer = () => {
     { id: 4, name: "Testimonials", url: "#testimonials" },
     { id: 5, name: "Get a Quote", url: "#booknow" },
   ];
+  const { displayName } = useEdmontonAreaDetection();
 
   return (
-    <>
-      <div className="bg-[#faf8f8fd] text-black mt-14 rounded-t-3xl flex justify-center items-center w-full">
-        <div data-="fade" className="container flex flex-col items-center">
-          <div className="logo">
-            {" "}
-            <Image width={150} height={150} src={logo} />
-          </div>
-          <h1 className="sm:text-3xl text-xl font-bold sm:text-left -translate-y-8">
-            Maple Leaf Cleaners Plus
-          </h1>
-          <span className="text-sm text-gray-700 -translate-y-6">
-            Shine Brighter with Us
-          </span>
-          <div className=" flex flex-wrap justify-center items-center w-full">
-            {/* Company details */}
-            <div className="py-8 px-4">
-              {/* Contact section */}
-              {/* <div>
-                <div className="flex items-center gap-3">
-                  <FaMobileAlt />
-                  <p>+1 (317) 275-9146</p>
-                </div>
-              </div> */}
-              {/* Social handles */}
+    <div className="bg-[#faf8f8fd] text-black mt-14 rounded-t-3xl flex justify-center items-center w-full">
+      <div data-="fade" className="container flex flex-col items-center">
+        <div className="logo">
+          <Image width={150} height={150} src={logo} alt="Maple Leaf Cleaners Plus Logo" />
+        </div>
+        <h1 className="sm:text-3xl text-xl font-bold sm:text-left -translate-y-8">
+          Maple Leaf Cleaners Plus
+        </h1>
+        <span className="text-sm text-gray-700 -translate-y-6">
+          Shine Brighter with Us
+        </span>
+        <div className="flex flex-wrap justify-center items-center w-full">
+          {/* Company details */}
+          <div className="py-8 px-4">
+            {/* Social handles */}
+            {displayName === "St. Albert" && (
               <Link
                 className="flex items-center gap-1 justify-center"
-                href={"https://www.facebook.com/profile.php?id=61565494418337"}
+                href="https://www.facebook.com/profile.php?id=61565494418337"
                 aria-label="Facebook"
+                target="_blank"
+                rel="noopener noreferrer"
               >
                 <FaFacebook className="text-black" />
                 Facebook page
               </Link>
-              <Link className="flex items-center gap-1 mt-3" href="/">
-                <FaGlobeAmericas />
-                https://mapleleafcleanersplus.com
-              </Link>
-              <Link className="flex items-center gap-1 mt-3" href={"/"}>
-                {" "}
-                <FaMailBulk /> mapleleafcleanersplus@gmail.com{" "}
-              </Link>
-            </div>
-            {/* Footer Links */}
-            <div className="flex">
-              <div>
-                <div className="py-8 px-4">
-                  <ul className="space-y-3">
-                    {links.map((link) => (
-                      <li
-                        key={link.id}
-                        className="hover:translate-x-1 duration-300"
-                      >
-                        <a
-                          href={link.url}
-                          className="cursor-pointer text-black hover:bg-clip-text hover:text-transparent hover:bg-gradient-to-r from-yellow-400 to-yellow-700"
-                        >
-                          {link.name}
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              </div>
-            </div>
+            )}
+            <Link 
+              className="flex items-center gap-1 mt-3" 
+              href="https://mapleleafcleanersplus.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaGlobeAmericas />
+              mapleleafcleanersplus.com
+            </Link>
+            <Link 
+              className="flex items-center gap-1 mt-3" 
+              href="mailto:mapleleafcleanersplus@gmail.com"
+            >
+              <FaMailBulk /> 
+              mapleleafcleanersplus@gmail.com
+            </Link>
           </div>
-          <div className="mb-1">
-            &copy; {new Date().getFullYear()} Maple Leaf Cleaners Plus
+          {/* Footer Links */}
+          <div className="flex">
+            <div className="py-8 px-4">
+              <ul className="space-y-3">
+                {links.map((link) => (
+                  <li
+                    key={link.id}
+                    className="hover:translate-x-1 duration-300"
+                  >
+                    <a
+                      href={link.url}
+                      className="cursor-pointer text-black hover:bg-clip-text hover:text-transparent hover:bg-gradient-to-r from-yellow-400 to-yellow-700"
+                    >
+                      {link.name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
+        <div className="mb-1">
+          &copy; {new Date().getFullYear()} Maple Leaf Cleaners Plus
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
